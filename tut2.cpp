eddce917aae34234ff7622a5746df8bf0dced364
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-05-02 13:51:51 -0700

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ int main(){
+   //int a = 20;
+   //int b = 20;
+
+   //\n is used to break the line 
+   int a = 20, b =  20;
+   float pi =3.1428;
+
+    cout<<"This is tutorial 2:\nThe value of a is: \n " <<a<< " \nThe value of  b is: \n"<<b;
+        cout<<" \nThe value of pi:\n"<<pi;
+
+        
+        
+        return 0;
+
+ }
