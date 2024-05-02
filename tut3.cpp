7c6da4fdd6ad7b54800cc2f48d84fc3233b51a6a
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-05-02 13:52:10 -0700

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+ 
+//  int sum1 = 150 + 150;
+//  int sum2 = sum1 + 100;
+//  int sum3 = sum2 + 150;
+
+//  cout<<"The value of sum1:\n"<<sum1<< "\nThe value of sum2:\n"<<sum2<< "\nThe value of sum3:\n"<<sum3;
+
+// int sum1 = 1000- 500;
+// int sum2 = sum1 + 10000;
+// int sum3 = sum2 * 10;
+// int sum4 = sum3 % 5;
+ 
+//  cout<<"The value of sum1:\n"<<sum1<< "\nThe value of sum2:\n"<<sum2<<"\nThe value of sum3:\n"<<sum3<<"\nThe value of sum4:\n"<<sum4;
+// bool isCodingFun = true;
+// bool isFishTasty = false;
+// cout << isCodingFun;  // Outputs 1 (true)
+// cout << isFishTasty;  // Outputs 0 (false)
+  
+// bool biryani = true;
+// bool palao = false;
+// cout<<biryani;
+
+
+    return 0;
+}
