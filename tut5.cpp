884b79a9c2dbd2380211fc1f75c7c177eed6259f
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-05-02 13:52:54 -0700

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std; // abu baker code
+int main(){
+    
+//     float a =10.23345;
+//     float b = 10.93345;
+//      float sum = a+b;
+//  cout<<"\nThe value of a is  :\n "  <<sum ;
+// 
+
+// This is the simple integar (when we store a huge value in simple integar form  our output must be over flow:)
+
+// int a = 10000003456, b = 200234587878, c = 398982045678;
+// int sum = a +b + c;
+// cout<<"\n Enter the value of "<<a;
+// cout<<"\n Enter the value of "<<b;
+// cout<<"\n Enter the value of "<<c;
+// cout<<"\nThe total value of a, b and c is   "<<sum;
+
+//This is a long integar 
+
+// long int a = 1002345638838 , b = 123456783843 ,c= 1234567845759;
+// long int sum = a +b + c;
+//  cout<<"\n Enter the value of "<<a;
+//  cout<<"\n Enter the value of "<<b;
+//  cout<<"\n Enter the value of "<<c;
+//  cout<<"\nThe total value of a, b and c is   "<<sum;
+
+
+return 0;
+}
