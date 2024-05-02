50d909774a6300009d27a6e93d9fc111234a7ca4
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-05-02 13:51:14 -0700

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//  int a; declaration
+//  a = 12; intialzation
+//   cout<<"size of int" <<sizeof (a)<<endl;
+
+//    float b;
+//   cout<<"size of float"<<sizeof (b)<<endl;
+   
+//    char c;
+//    cout<<"size of char"<<sizeof(c)<<endl;
+
+// bool d;
+// cout<<"size of bool"<<sizeof(d)<<endl;
+int sum =10;                      //aik  integar container banaya ha jisme humne 10 ko store karwa diya ha 
+cout<< "hello world "<<sum;        // print karne ke liye cout use kiya jata ha 
+
+    return 0;
+    
+}
