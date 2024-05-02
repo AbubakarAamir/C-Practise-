71e0e6da234723b8a6a16b5234eae48ca523ebb8
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-05-02 13:52:33 -0700

    Add files via upload

---
+++ tut4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+
+{
+    /*int  a = 100, b = 200;
+    float pi = 3.14;
+    char c = 'u' ;
+    cout<<"This is tutorial 4\n Here the vale of a is: \n"<<a<<"\nAnd here the value of b: is \n"<<b;
+    cout<<"\nThe value of pi is :\n"<<pi;
+    cout<<"\nThe value of c is :\n"<<c;
+
+
+return 0;*/
+
+}
+
+
+
+
